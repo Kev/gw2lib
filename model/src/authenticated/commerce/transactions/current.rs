aeeb539125d80cf9a65ef6f569272014cae6b0bd
Kevin Smith <git@kismith.co.uk>
2022-09-24 23:06:03 +0100

    Move transactions up a level

--- model/src/authenticated/commerce/transactions/current.rs
+++ model/src/authenticated/commerce/transactions/current.rs
@@ -1,2 +1,42 @@
-pub mod buys;
-pub mod sells;
+use crate::{items::ItemId, Endpoint, FixedEndpoint, TimeStamp};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Buy {
+    pub id: u64,
+    pub item_id: ItemId,
+    pub price: u64,
+    pub quantity: u64,
+    pub created: TimeStamp,
+}
+
+pub type Buys = Vec<Buy>;
+
+impl Endpoint for Buys {
+    const AUTHENTICATED: bool = true;
+    const LOCALE: bool = false;
+    const URL: &'static str = "v2/commerce/transactions/current/buys";
+    const VERSION: &'static str = "2022-09-22T00:00:00.000Z";
+}
+
+impl FixedEndpoint for Buys {}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Sell {
+    pub id: u64,
+    pub item_id: ItemId,
+    pub price: u64,
+    pub quantity: u64,
+    pub created: TimeStamp,
+}
+
+pub type Sells = Vec<Sell>;
+
+impl Endpoint for Sells {
+    const AUTHENTICATED: bool = true;
+    const LOCALE: bool = false;
+    const URL: &'static str = "v2/commerce/transactions/current/sells";
+    const VERSION: &'static str = "2022-09-22T00:00:00.000Z";
+}
+
+impl FixedEndpoint for Sells {}
